320af1307ff0dad6e771e7fc33896581a5a00b68
Gallay Lajos <gallay.lajos@gmail.com>
2025-01-30 11:21:29 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  Serial.begin(115200
+  );
+  Serial.println("CYD Init!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
